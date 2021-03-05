6b5a6bb29a124e34e4e054818d19f36e75c11078
shiannejt <shianne@gmetrix.com>
2021-03-05 13:38:47 -0700

    probably done

---
+++ assets/js/script.js
@@ -0,0 +1,25 @@
+const displayGifs = () => {
+    const giphyKey = "jeNtNzOLOIjvgsW8XsqubMLndTig1AXs";
+
+    let queryURL = "https://api.giphy.com/v1/gifs/search?q=pandas&api_key=" + giphyKey;
+    let gifNum = Math.floor(Math.random() * 15);
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+      }).then(function (res) {
+        let results = res.data;
+  
+        //let gifDiv = $("<div>");
+        let charGif = $("<img>");
+        charGif.attr("src", results[gifNum].images.original.url);
+  
+        //gifDiv.prepend(charGif);
+        $("#pandaBox").prepend(charGif);
+      });
+};
+
+// Handle button click
+$(".startBtn").click(function() {
+    displayGifs();
+});
